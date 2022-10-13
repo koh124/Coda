acd1b1116422979b90228bbb49b16010337e4bf0
KoheiMori <koheimori@Mths20832.local>
2022-10-13 16:01:08 +0900

    update CodeControlelr Class

--- coda/models/models.py
+++ coda/models/models.py
@@ -1,9 +1,4 @@
 from django.db import models
-from .code.codeinterface import CodeInterface
-from .docker.docker import DockerInterface
-from .languages import LANGUAGES
-from datetime import datetime
-import os
 
 # Create your models here.
 
@@ -45,61 +40,24 @@ class Language(models.Model):
 class Article(models.Model):
   title = models.CharField(max_length=50) # 記事タイトル
   body = models.TextField() # 記事本文
-  lang_id = models.ForeignKey(Language, on_delete=models.CASCADE)
-
-example = {
-  'lang': 'python',
-  'code': 'print("hello coda!!")'
-}
+  language = models.ForeignKey(Language, on_delete=models.CASCADE)
 
 class Code(models.Model):
   module_name = models.CharField(max_length=50) # SayHello
   language = models.CharField(max_length=50) # python
   code = models.TextField() # print("Hello")
-  file_name = models.CharField() # 20221212_something_11.py
+  file_name = models.CharField(max_length=50) # 20221212_something_11.py
   file_path = models.URLField() # path/to/file
   web_client_lang = models.BooleanField(default=False) # False
-  user_id = models.ForeignKey(User, on_delete=models.CASCADE)
-  article_id = models.ForeignKey(Article, on_delete=models.CASCADE)
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
+  article = models.ForeignKey(Article, on_delete=models.CASCADE)
   created_at = models.DateTimeField(auto_now_add=True)
   updated_at = models.DateTimeField(auto_now=True)
-
-  def __init__(self, lang=example['lang'], code=example['code']) -> None:
-    self.language = lang
-    self.code = code
-    self.created_at = datetime.now()
-
-  def getLang(self):
-    return self.language
-
-  def writeFile(self):
-    # ファイル名: フォーマット
-    # YYYYMMDDHHmmSS_userid_1111.lang
-    date = self.created_at
-    self.file_name = str(date.year) + str(date.month) + str(date.day) + str(date.hour) + str(date.minute) + str(date.second) + '_' + '1' + '_' + '1111' + '.' + LANGUAGES[self.language]
-    self.file_path = os.path.join('coda/models/codes/user1', self.file_name)
-
-    with open(self.file_path, 'w', encoding='utf-8') as file: # 一時的にファイルオブジェクトを作り、書き込みを行う
-      file.write(self.code)
-
-    return self
-
-  def exec(self, docker:DockerInterface): # 依存注入できるようにした（スタブを入れてテストできる）
-    docker.run()
 
 class Code_Conf(models.Model):
   public = models.BooleanField(default=False)
   executable = models.BooleanField(default=False)
-  code_id = models.ForeignKey(Code, on_delete=models.CASCADE)
-
-class Film(models.Model):
-  name = models.CharField(max_length=100)
-  publication_date = models.DateTimeField('date published')
-
-class Theater(models.Model):
-  film = models.ForeignKey(Film, on_delete=models.CASCADE) # 外部キー
-  name = models.CharField(max_length=100)
-  area = models.CharField(max_length=100)
+  code = models.ForeignKey(Code, on_delete=models.CASCADE)
 
 # ・python3 manage.py makemigrations coda
 # モデルに記述したDBスキーマを元にマイグレーションファイルを作成する
--- coda/views.py
+++ coda/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models.models import Code
+from .models.models import Article, Code, Language, User
 from .models.docker.docker import Docker
+from .models.docker.dockerinterface import DockerInterface
+from .models.languages import LANGUAGES
+from datetime import datetime
+import os
 
 # import debugpy
 # port = 5678
@@ -15,10 +19,28 @@ def index(request):
   post = ['php', '<?php \necho("hi!! Im come from view!");']
   post = ['python', "import math\nprint(math.sqrt(81))"] # モジュールはインストールしないと使えない
 
-  code = Code(lang=post[0], code=post[1])
+  User(name='koh').save()
+  Language(name='python').save()
+  Article(title='pythonで標準出力する方法', body='pythonで標準出力する方法を紹介します', language=Language(id=1)).save()
+
+  Code(
+    module_name = "SayHello",
+    language = "python",
+    code = 'print("Hello")',
+    file_name = "20221212_something_11.py",
+    file_path = "path/to/file",
+    web_client_lang = False,
+    user = User(id=1),
+    article = Article(id=1),
+    created_at = datetime.now(),
+    updated_at = datetime.now()
+  ).save()
+
+  code = CodeController(lang=post[0], code=post[1])
   code.writeFile()
 
   output = Docker(code).run()
+  print(output)
 
   # パラメータ受け取り
   print(request.POST) # postパラメータ受け取り
@@ -45,3 +67,49 @@ def djangoTemplateLanguage(request):
     'array': ['coffee', 'orange juice', 'vegetables']
   }
   return render(request, 'dtl.html', param) # viewに変数を渡す
+
+example = {
+  'lang': 'python',
+  'code': 'print("hello coda!!")'
+}
+
+# Codeモデルを継承
+class CodeController(Code):
+  module_name = "SayHello"
+  language = "python"
+  code = 'print("Hello")'
+  file_name = "20221212_something_11.py"
+  file_path = ""
+  web_client_lang = False
+  user = 1
+  article = 1
+  created_at = datetime.now()
+  updated_at = datetime.now()
+
+  # オブジェクト生成した瞬間にレコードが1件追加されるのか？
+  # case: 下書き、オンラインエディタとして実行
+  # →ファイル生成やpath設定を行っていい
+  # →実行したいだけなら？
+  # →レコードはいらない
+  def __init__(self, lang=example['lang'], code=example['code']) -> None:
+    self.language = lang
+    self.code = code
+    self.created_at = datetime.now()
+
+  def getLang(self):
+    return self.language
+
+  def writeFile(self):
+    # ファイル名: フォーマット
+    # YYYYMMDDHHmmSS_userid_1111.lang
+    date = self.created_at
+    self.file_name = str(date.year) + str(date.month) + str(date.day) + str(date.hour) + str(date.minute) + str(date.second) + '_' + '1' + '_' + '1111' + '.' + LANGUAGES[self.language]
+    self.file_path = os.path.join('coda/models/codes/user1', self.file_name)
+
+    with open(self.file_path, 'w', encoding='utf-8') as file: # 一時的にファイルオブジェクトを作り、書き込みを行う
+      file.write(self.code)
+
+    return self
+
+  def exec(self, docker:DockerInterface): # 依存注入できるようにした（スタブを入れてテストできる）
+    docker.run()
